9c24c4617033b7b2d7d1636a6699e364f5ab28b3
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-11-01 14:12:43 -0700

    adding .net client sample

---
+++ samples/frontend/dotnet/Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using Client.Extensions;
+
+using Console;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder b = new(args);
+b.Configuration
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables("AICHATPROTOCOL_")
+    .AddCommandLine(args);
+
+b.Services
+    .AddHostedService<Repl>()
+    .AddAiChatProtocolClient();
+
+b.Build().Run();
---
+++ samples/frontend/dotnet/Console/Repl.cs
@@ -0,0 +1,36 @@
+﻿namespace Console;
+
+using Backend.Model;
+
+using Client.Interfaces;
+
+using Microsoft.Extensions.Hosting;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal class Repl(IAiChatProtocolClient _protocolClient) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Console.Write("Enter your message: ");
+            var message = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                Console.WriteLine("Message cannot be empty.");
+                continue;
+            }
+
+            var request = new AIChatRequest([new() { Content = message }]);
+            var response = await _protocolClient.CompleteAsync(request, cancellationToken);
+
+            Console.WriteLine($"Response: {response.Message.Content}");
+            Console.WriteLine();
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
